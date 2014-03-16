50409a6649612deef1736849f061fe74282a1acc
jorgeer <jfemail3@gmail.com>
2014-03-16 17:34:38 +0100

    SOMETHING works!

--- gulpfile.js
+++ gulpfile.js
@@ -9,54 +9,42 @@ var concat = require('gulp-concat');
 var clean = require('gulp-clean');
 
 gulp.task('lint', function() {
-  gulp.src('./app/app/app.js')
+  gulp.src('./app/src/js/app.js')
     .pipe(jshint())
     .pipe(jshint.reporter('default'));
 });
 
 gulp.task('clean', function() {
-    gulp.src('./app/js/app.js', {read: false})
+    gulp.src('./app/dist/js/*.js', {read: false})
         .pipe(clean());
 });
 
+
 gulp.task('browserify', function() {
-    gulp.src(['./app/app/*.js'])
-        .pipe(browserify({
-          insertGlobals : true,
-          debug : true
-        }))
-        .pipe(concat('app.js'))
-        .pipe(gulp.dest('./app/js'))
+    gulp.src(['./app/src/js/*.js'])
+        .pipe(concat('bundle.js'))
+        .pipe(gulp.dest('./app/dist/js/'));
 });
 
-gulp.task('minify-css', function() {
-  gulp.src('./static/css/*.css')
-    .pipe(minifyCSS(opts))
-    .pipe(gulp.dest('./dist/'))
+gulp.task('consolidate', function() {
+    gulp.src(['./app/src/js/*.js',
+      './bower_components/angular/angular.js',
+      './bower_components/angular-route/angular-route.js'])
+        .pipe(gulp.dest('./app/dist/js/'));
+});
+
+gulp.task('fix', function() {
+    gulp.src([ './app/dist/js/unfixed_bundle.js', './app/src/js/*.js',])
+        .pipe(concat('bundle.js'))
+        .pipe(gulp.dest('./app/dist/js/'));
 });
 
 gulp.task('minify-html', function() {
-  gulp.src('./static/html/*.html')
+  gulp.src('./app/src/templates/*.html')
     .pipe(minifyHTML(opts))
-    .pipe(gulp.dest('./dist/'))
-});
-
-gulp.task('imagemin', function () {
-    gulp.src('src/image.png')
-        .pipe(imagemin())
-        .pipe(gulp.dest('dist'));
-});
-
-//TODO add support for source maps
-gulp.task('minify-js', function() {
-  gulp.src('./app/js/*.js')
-    .pipe(uglify({
-    	// inSourceMap: 
-    	// outSourceMap: "app.js.map"
-    }))
-    .pipe(gulp.dest('./app/js'))
+    .pipe(gulp.dest('./app/dist/templates/'));
 });
 
 gulp.task('default', function(){
-  gulp.run('clean', 'browserify', 'lint');
+  gulp.run('clean', 'consolidate', 'minify-html', 'lint');
 });
